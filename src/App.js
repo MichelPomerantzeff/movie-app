859c2d19a7b6122b91f085dd5c245b71b11f235b
Michel Pomerantzeff <micheldauniao@hotmail.com>
2022-07-17 19:46:44 +0100

    added movie information component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import Topbar from "./components/Topbar";
 import SearchField from "./components/SearchField";
 import MovieList from "./components/MovieList";
+import MovieInfo from "./components/MovieInfo";
 // import MovieInfo from "./components/MovieInfo";
 // import Footer from "./components/Footer";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 
 function App() {
 
-  const API_Key = "3b08cc46"
-
   const [search, setSearch] = useState([])
   const [movieName, setMovieName] = useState()
-  const [movieListData, setMovieListData] = useState([])
-  // const [movieInfoData, setMovieInfoData] = useState([])
+  const [movieID, setMovieID] = useState()
 
-  useEffect(() => {
-    fetch(`https://www.omdbapi.com/?s=${movieName}&apikey=${API_Key}`)
-      .then(response => response.json())
-      .then(res => setMovieListData(res.Search))
-  }, [movieName])
-
+  // Get value from input field
   function handleSearch(e) {
     setSearch(e.target.value)
   }
 
+  // Assign input field value to movieName variable and send that value to movieList Component.
+  // This way there wont be too many requests while the user is typing on the input field.
   function searchMovie() {
     setMovieName(search)
   }
+
+
+
+  function incomingValues(movie) {
+    setMovieID(movie[0].imdbID)
+  }
+
 
 
   return (
@@ -40,25 +42,14 @@ function App() {
           handleSearch={handleSearch}
           searchMovie={searchMovie}
         />
+        <MovieInfo
+          movieID={movieID}
+        />
+        <MovieList
+          movieName={movieName}
+          info={incomingValues}
+        />
 
-        <div className="movieContainer">
-          {
-            movieName ? movieListData.map((movie, index) => {
-
-              return (
-                  <MovieList   
-                    key={index}
-                    movieName={movie.Title}
-                    poster={movie.Poster}
-                    type={movie.Type}
-                    year={movie.Year}
-                  />
-              )
-              
-            }) 
-            : <div className="emptyField">No Movies showing</div>
-          }
-        </div>
 
         {/* <MovieList /> */}
         {/* <Footer /> */}
--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react"
+
 import "./MovieInfo.css"
 
+
 function MovieInfo(props) {
-    
+
+    const [infoData, setInfoData] = useState([])
+
+    useEffect(() => {
+        fetch(`https://www.omdbapi.com/?i=${props.movieID}&apikey=3b08cc46`)
+            .then(response => response.json())
+            .then(res => setInfoData(res))
+    }, [props.movieID])
+
+
     return (
 
-        <div>
-            Information of the movie that was clicked
+        <div className="movieInformation">
+            <div>
+                <img src={infoData.Poster} alt="Poster" />
+            </div>
+            <div className="writtingInfo">
+                <span><strong>Rating:</strong> {infoData.imdbRating}</span>
+                <span><strong>Year:</strong> {infoData.Year}</span>
+                <span><strong>Language:</strong> {infoData.Language}</span>
+                <span><strong>Release date:</strong> {infoData.Released}</span>
+                <span><strong>Runtime:</strong> {infoData.Runtime}</span>
+                <span><strong>Genre:</strong> {infoData.Genre}</span>
+                <span><strong>Director:</strong> {infoData.Director}</span>
+                <span><strong>Actors:</strong> {infoData.Actors}</span>
+                <span><strong>Plot:</strong> {infoData.Plot}</span>
+            </div>
         </div>
 
 
